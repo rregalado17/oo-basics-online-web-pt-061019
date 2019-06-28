b14780ee7a69b963f5cb8f8668a0ff3f914da4d3
magical-flow-3186 <rregalado17@gmail.com>
2019-06-28 21:53:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
-
+class Book
+  def initialize (title)
+    @title = title 
+  end
+  
+  def title
+    @title 
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  attr_accessor :genre, :page_count
+  
+  def turn_page
+  end 
+  
+end 
 
